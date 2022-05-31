d4519df483521103c68fc72f21c60f90fae969f1
FlutLab.io <>
2022-05-31 05:16:46 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -3,6 +3,31 @@ import 'package:flutter/material.dart';
 
 class Chats extends StatelessWidget {
   const Chats({Key? key}) : super(key: key);
+  Widget getContainer(text) {
+    return Container(
+      padding: EdgeInsets.only(left: 17, top: 7),
+      color: Colors.black12,
+      height: 35,
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.grey.shade600),
+      ),
+    );
+  }
+
+  Widget getStory(title, subTitle) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage("https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
+        radius: 30,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+      ),
+      subtitle: Text(subTitle),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
