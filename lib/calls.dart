37d0a2639565070f10699cefc726f61771a2f6e9
FlutLab.io <>
2022-05-31 07:39:05 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Calls extends StatelessWidget {
   const Calls({Key? key}) : super(key: key);
+
   // Masukkan coding disini
   @override
   Widget build(BuildContext context) {
@@ -18,14 +19,16 @@ class Calls extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-
-            // Masukkan Coding disini jangan rubah lainnya
-            //
-            //
-            //
-            //
-            //
-            //
+            callPhone("Sinta", Icons.call_received, Colors.green, "Jan 13, 02:13", NetworkImage("https://i.postimg.cc/T2HT1Lcc/2022-05-31-at-13-21-54.jpg")),
+            callPhone("Awaliya", Icons.call_made, Colors.green, "Feb 08, 10:30", NetworkImage("https://i.postimg.cc/rpd5xkL0/01.jpg")),
+            callPhone("Devy", Icons.call_received, Colors.green, "Feb 17, 05:20", NetworkImage("https://i.postimg.cc/QNW9bbPP/nail-art-lucu-dari-bunga-bunga-kering-yang-terlihat-manja-Mye-KMRr-W1k.jpg")),
+            callPhone("Gita", Icons.call_made, Colors.green, "Maret 25, 08:25", NetworkImage("https://i.postimg.cc/6pVCh07w/Image-2022-05-31-at-13-21-54.jpg")),
+            callPhone("Hisyam", Icons.call_received, Colors.green, "April 22, 12:34", NetworkImage("https://i.postimg.cc/3wxWvMPM/panorama-alam-terasiring-di-desa-pendoworejo-20170608-223549.jpg")),
+            callPhone("Furqonal", Icons.call_made, Colors.red, "April 25, 13:07", NetworkImage("https://i.postimg.cc/8PdCXPGQ/images-1.jpg")),
+            callPhone("Gabriel", Icons.call_made, Colors.red, "Juni 01, 09:15", NetworkImage("https://i.postimg.cc/0yQqG82C/gambar1.jpg")),
+            callPhone("Sumartatik", Icons.call_received, Colors.red, "Juli 12, 05:25", NetworkImage("https://i.postimg.cc/rpd5xkL0/01.jpg")),
+            callPhone("Hisyam", Icons.call_received, Colors.red, "Agustus 17, 02:00", NetworkImage("https://i.postimg.cc/3wxWvMPM/panorama-alam-terasiring-di-desa-pendoworejo-20170608-223549.jpg")),
+            callPhone("Hisyam", Icons.call_received, Colors.red, "Agustus 19, 03:04", NetworkImage("https://i.postimg.cc/3wxWvMPM/panorama-alam-terasiring-di-desa-pendoworejo-20170608-223549.jpg")),
           ],
         ),
       ),
@@ -53,6 +56,41 @@ class Calls extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget callPhone(String name, IconData iconSitus, Color iconColor, String Time, NetworkImage image) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 0.9),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: image,
+          radius: 40,
+        ),
+        title: Text(
+          name,
+          style: TextStyle(fontWeight: FontWeight.w700),
+        ),
+        subtitle: Row(
+          children: [
+            Icon(
+              iconSitus,
+              color: iconColor,
+              size: 20,
+            ),
+            SizedBox(width: 6),
+            Text(
+              Time,
+              style: TextStyle(fontSize: 13),
+            ),
+          ],
+        ),
+        trailing: Icon(
+          Icons.call,
+          size: 28,
+          color: Colors.teal,
+        ),
       ),
     );
   }
--- lib/chats.dart
+++ lib/chats.dart
@@ -29,7 +29,7 @@ class Chats extends StatelessWidget {
               subtitle: Row(
                 children: [
                   Icon(
-                    Icons.access_alarm,
+                    Icons.done_all_rounded,
                     color: Colors.blue,
                   ),
                   SizedBox(
@@ -63,6 +63,37 @@ class Chats extends StatelessWidget {
               ),
               trailing: Text('18.00'),
             ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage("https://i.postimg.cc/T2HT1Lcc/2022-05-31-at-13-21-54.jpg"),
+                radius: 30,
+              ),
+              title: Text(
+                'Devi',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Row(
+                children: [
+                  Icon(
+                    Icons.done_all_rounded,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 3.0,
+                  ),
+                  Text('Ujian nya sudah selesai'),
+                ],
+              ),
+              trailing: Text('05.10'),
+            ),
+            chatPhone("Hisyam", "Dimana posisi?", "07:06", Icons.done_all, Colors.blue, NetworkImage("https://i.postimg.cc/3wxWvMPM/panorama-alam-terasiring-di-desa-pendoworejo-20170608-223549.jpg")),
+            chatPhone("Sumartatik", "Besok jadi ?", "12:24", Icons.done, Colors.grey, NetworkImage("https://i.postimg.cc/0yQqG82C/gambar1.jpg")),
+            chatPhone("Gabriel", "Iya kesana", "10:18", Icons.done_all, Colors.grey, NetworkImage("https://i.postimg.cc/QC32F7Mf/download.jpg")),
+            chatPhone("Furqonal", "Besok les ya", "08:04", Icons.done, Colors.grey, NetworkImage("https://i.postimg.cc/8PdCXPGQ/images-1.jpg")),
+            chatPhone("Awaliya", "Hehehhe", "22:12", Icons.done, Colors.grey, NetworkImage("https://i.postimg.cc/rpd5xkL0/01.jpg")),
+            chatPhone("Devy", "Ketemu di perpus", "18:02", Icons.done, Colors.grey, NetworkImage("https://i.postimg.cc/dVk5K1s3/boneka-kucing-ukuran-besar-simulasi-dengan-suara-untuk-anak-anak.jpg")),
+            chatPhone("Hisyam", "Udah ngerjakan tugas nya ?", "11:23", Icons.done_all, Colors.blue, NetworkImage("https://i.postimg.cc/6pVCh07w/Image-2022-05-31-at-13-21-54.jpg")),
+            chatPhone("Gita", "Cara nya membuat anyamannya gimana?", "11:14", Icons.done_all, Colors.blue, NetworkImage("https://i.postimg.cc/4y0p86h5/145951-tedy-bear.jpg")),
           ],
         ),
       ),
@@ -73,6 +104,37 @@ class Chats extends StatelessWidget {
           Icons.chat_bubble,
           color: Colors.white,
         ),
+      ),
+    );
+  }
+
+  Widget chatPhone(String name, String des, String time, IconData iconSitus, Color iconColor, NetworkImage img) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 0.9),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: img,
+          radius: 30,
+        ),
+        title: Text(
+          name,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Row(children: [
+          Icon(
+            iconSitus,
+            size: 16,
+            color: iconColor,
+          ),
+          SizedBox(width: 5),
+          Text(
+            des,
+            style: TextStyle(
+              fontSize: 13,
+            ),
+          ),
+        ]),
+        trailing: Text(time),
       ),
     );
   }
